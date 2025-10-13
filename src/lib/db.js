fffdd3913196ffd6f3906d929887992ba9449d52
mamutstar <blackhole1369@gmail.com>
2025-10-13 20:13:15 +0330

    database change user in db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,13 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
+  // host: "localhost",   // یا آدرس سرور دیتابیس
+  // user: "root",        // یوزرنیم MySQL
+  // password: "",        // پسورد MySQL
+  // database: "qvqmnext" // اسم دیتابیس
+
   host: "localhost",   // یا آدرس سرور دیتابیس
-  user: "root",        // یوزرنیم MySQL
-  password: "",        // پسورد MySQL
+  user: "qavamuser2",        // یوزرنیم MySQL
+  password: "9176298900@Qavam",        // پسورد MySQL
   database: "qvqmnext" // اسم دیتابیس
 });
