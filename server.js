3731f2486240152fe4375ddcdcf2b80885742fd1
mamutstar <blackhole1369@gmail.com>
2025-11-02 13:25:37 +0330

    return change server.js and limit add

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + "-" + file.originalname);
   }
 });
-const upload = multer({ storage });
+const upload = multer({
+   storage,
+  limits: { fileSize: 20 * 1024 * 1024 } // 20MB
+  });
 
 app.prepare().then(() => {
   const server = express();
